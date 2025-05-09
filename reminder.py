b6f0cb01a6152d3f715e1f65c22c531e38412e57
iqnite <iqnitetechnologies@gmail.com>
2025-05-09 10:19:30 +0200

    Update strings

--- reminder.py
+++ reminder.py
@@ -1,9 +1,8 @@
-'''
+"""
 Startup Reminder
 Philipp D.
 Jun. 2023
-'''
-
+"""
 
 import tkinter as tk
 from tkinter.messagebox import showinfo, showerror, showwarning
@@ -14,14 +13,14 @@ class Config(object):
         self.file_name = file_name
 
     def __enter__(self):
-        self.file = open(self.file_name, 'w')
+        self.file = open(self.file_name, "w")
         return self.file
 
     def __exit__(self, *args):
         self.file.close()
 
 
-class ReminderApp (tk.Frame):
+class ReminderApp(tk.Frame):
     def __init__(self, parent: tk.Tk):
         super().__init__(parent)
         self.pack(fill="both", expand=True)
@@ -31,8 +30,7 @@ class ReminderApp (tk.Frame):
 
     def def_gui(self):
         self.remindersFrame = tk.LabelFrame(self, text="Active reminders")
-        self.remindersBox = tk.Listbox(
-            self.remindersFrame, selectmode="browse")
+        self.remindersBox = tk.Listbox(self.remindersFrame, selectmode="browse")
 
         self.optionsFrame = tk.Frame(self)
         self.addFrame = tk.LabelFrame(self.optionsFrame, text="Add reminder")
@@ -45,25 +43,27 @@ class ReminderApp (tk.Frame):
         self.addNameLabel = tk.LabelFrame(self.addFrame, text="Name")
         self.addName = tk.StringVar(self.addNameLabel)
         self.addNameInput = tk.Entry(self.addNameLabel, textvariable=self.addName)
-        self.addButton = tk.Button(
-            self.addFrame, text="Add", command=self.add_to_list)
+        self.addButton = tk.Button(self.addFrame, text="Add", command=self.add_to_list)
 
-        self.selectFrame = tk.LabelFrame(
-            self.optionsFrame, text="Selected reminder")
+        self.selectFrame = tk.LabelFrame(self.optionsFrame, text="Selected reminder")
         self.removeButton = tk.Button(
-            self.selectFrame, text="Remove", command=self.remove_from_list)
-        self.copyButton = tk.Button(
-            self.selectFrame, text="Copy", command=self.copy)
+            self.selectFrame, text="Remove", command=self.remove_from_list
+        )
+        self.copyButton = tk.Button(self.selectFrame, text="Copy", command=self.copy)
 
-        self.intervalFrame = tk.LabelFrame(
-            self.optionsFrame, text="Notification")
+        self.intervalFrame = tk.LabelFrame(self.optionsFrame, text="Notification")
         self.intervalInfo = tk.Label(
-            self.intervalFrame, text="Set how many days before the given date the app should remind you.\nReminders will only appear at startup.", wraplength=window.winfo_width()/3)
+            self.intervalFrame,
+            text="Set how many days before the given date the app should remind you.\nReminders will only appear at startup.",
+            wraplength=window.winfo_width() / 3,
+        )
         self.intervalDays = tk.StringVar(self.intervalFrame)
         self.intervalInput = tk.Entry(
-            self.intervalFrame, textvariable=self.intervalDays)
+            self.intervalFrame, textvariable=self.intervalDays
+        )
         self.intervalButton = tk.Button(
-            self.intervalFrame, text="Save & check", command=self.verify)
+            self.intervalFrame, text="Save & check", command=self.verify
+        )
 
     def build_gui(self):
         self.remindersFrame.pack(side="left", fill="both", expand=True)
@@ -91,8 +91,10 @@ class ReminderApp (tk.Frame):
     def add_to_list(self):
         name = self.addName.get()
         if ("@" in name) or ('"' in name):
-            showerror(title="Could not add reminder",
-                      message="Name cannot contain the following characters: @ \".")
+            showerror(
+                title="Could not add reminder",
+                message='Name cannot contain the following characters: @ "',
+            )
             return
         try:
             day = int(self.addDay.get())
@@ -100,11 +102,12 @@ class ReminderApp (tk.Frame):
         except:
             showerror(title="Could not add reminder", message="Invalid date.")
             return
-        if (not month in range(1, 13, 1)
-                or not day in range(1, 32, 1)
-                or ((month == 4 or month == 6 or month == 9 or month == 11) and day > 30)
-                or (month == 2 and day > 29)
-            ):
+        if (
+            not month in range(1, 13, 1)
+            or not day in range(1, 32, 1)
+            or ((month == 4 or month == 6 or month == 9 or month == 11) and day > 30)
+            or (month == 2 and day > 29)
+        ):
             showerror(title="Could not add reminder", message="Invalid date.")
             return
         self.remindersBox.insert(1, f"{name} @ {day}. {month}")
@@ -126,30 +129,37 @@ class ReminderApp (tk.Frame):
             try:
                 int(self.intervalInput.get())
             except:
-                showerror("Could not set interval",
-                          message="Invalid interval number.")
+                showerror("Could not set interval", message="Invalid interval number.")
                 return
-            xprofile.write(
-                f"\nINTERVAL = {abs(int(self.intervalInput.get()))}")
+            xprofile.write(f"\nINTERVAL = {abs(int(self.intervalInput.get()))}")
 
     def verify(self):
         self.save()
         try:
             import remind_startup as run
         except ModuleNotFoundError:
-            showerror(title="An error occurerd", message="Your settings were saved, but could not check for reminders because the module 'remind_startup.py' was not found in the current directory. Try downloading the program again.")
+            showwarning(
+                title="An error occurerd",
+                message="Your settings were saved, but could not check for reminders because the module 'remind_startup.py' was not found in the current directory. Try downloading the program again.",
+            )
         else:
             try:
                 run.main()
             except NameError:
-                showerror(title="An error occurerd",
-                          message="Your settings were saved, but could not check for reminders because the autorun module is corrupted. Try downloading the program again.")
+                showwarning(
+                    title="An error occurerd",
+                    message="Your settings were saved, but could not check for reminders because the autorun module is corrupted. Try downloading the program again.",
+                )
             except:
-                showwarning(title="An error occurerd",
-                            message="An error occured while saving your settings. Please try again.")
+                showwarning(
+                    title="An error occurerd",
+                    message="An error occured while saving your settings. Please try again.",
+                )
             else:
                 showinfo(
-                    title="Success", message="Settings saved and reminders checked successfully.")
+                    title="Success",
+                    message="Settings saved and reminders checked successfully.",
+                )
 
     @staticmethod
     def get_date(item):
@@ -158,18 +168,17 @@ class ReminderApp (tk.Frame):
         date_tuple = reminder_tuple[1].split(".")
         day = date_tuple[0]
         month = date_tuple[1]
-        return {
-            "name": name,
-            "day": day,
-            "month": month
-        }
+        return {"name": name, "day": day, "month": month}
 
     def compile(self):
         result = []
         for i in self.remindersBox.get(0, "end"):
             tp = self.get_date(i)
             result.append(
-                "{" + f'"name": "{tp["name"].strip(" ")}", "day": "{tp["day"]}", "month": "{tp["month"]}"' + '},')
+                "{"
+                + f'"name": "{tp["name"].strip(" ")}", "day": "{tp["day"]}", "month": "{tp["month"]}"'
+                + "},"
+            )
         result_str = "REMINDERS = ["
         for j in result:
             result_str += j
@@ -190,9 +199,11 @@ class ReminderApp (tk.Frame):
     def load(self):
         try:
             import profile_1 as conf
+
             for i in conf.REMINDERS:
                 self.remindersBox.insert(
-                    1, f"{i['name']} @ {int(i['day'])}. {int(i['month'])}")
+                    1, f"{i['name']} @ {int(i['day'])}. {int(i['month'])}"
+                )
             self.intervalDays.set(str(conf.INTERVAL))
         except:
             self.intervalDays.set(str(1))
@@ -202,5 +213,5 @@ window = tk.Tk()
 window.title("Startup Reminder - Settings")
 window.geometry("500x600")
 window.state("zoomed")
-Main = ReminderApp(window)
-Main.mainloop()
+main = ReminderApp(window)
+main.mainloop()
